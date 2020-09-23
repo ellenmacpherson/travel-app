7cb27e9fb6664e3f25ae3af6eb8c79025ee8dd3b
Robert Webster <rwhwebster@gmail.com>
2020-09-23 23:00:10 +0100

    Switched up app.js to use the restana-based code

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
+// The main .js file for the app => launches the server.
 require('dotenv').config()
+
+var path = require('path')
+// var fs = require('fs');
+const files = require('serve-static')
 const http = require('http');
-var fs = require('fs');
+const morgan = require('morgan');
+const restana = require('restana');
 
 const hostname = process.env.APP_HOST;
 const port = process.env.APP_PORT;
 
-//Boot up the API/backend
-const API = require('./src/api/server.js');
+const service = restana(); // init the API
+const backend = require('./src/api/routers'); // Init API router which connects database stuff
+service.use(morgan('combined')); // Set default middleware
+service.use('/db', backend); // add database API methods to /db path
 
-fs.readFile('index.html', (err, html) => {
-  if (err) {
-    throw err;
-  }
-  const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/html');
-    res.write(html);
-    res.end();
-  });
-  
-  server.listen(port, hostname, () => {
+const frontend = files(__dirname); // Using serve-static to serve webpages with restana
+
+service.use('/', frontend) // Use webpages at API root.
+
+// service.get('/', async (req, res) => {
+//     fs.readFile('index.html', (err, html) => {
+//         res.statusCode = 200;
+//         res.setHeader('Content-type', 'text/html');
+//         res.write(html);
+//         res.end();
+//         res.
+//     });
+// });
+
+//service.start(process.env.API_PORT).then((server) => {});
+
+const server = http.createServer(service.callback());
+server.listen(port, hostname, () => {
     console.log(`App server started on http://${hostname}:${port}/`);
-  });
 });
