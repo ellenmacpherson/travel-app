daca1825e9b589ffc00c41dd352dff41c62001b3
Robert Webster <rwhwebster@gmail.com>
2020-10-04 15:23:39 +0100

    Some cleanup

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // The main .js file for the app => launches the server.
-require('dotenv').config()
+const dotenv = require('dotenv');
+dotenv.config();
 
-var path = require('path')
-// var fs = require('fs');
+const path = require('path')
 const files = require('serve-static')
 const http = require('http');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); // Required for POST methods
 const morgan = require('morgan');
 const restana = require('restana');
 
@@ -13,32 +13,13 @@ const hostname = process.env.APP_HOST;
 const port = process.env.APP_PORT;
 
 const service = restana(); // init the API
-const backend = require('./src/api/routers'); // Init API router which connects database stuff
+const backend = require('./api/routers'); // Init API router which connects database stuff
 service.use(bodyParser.urlencoded({extended: false}));
 service.use(morgan('short')); // Set default middleware
 service.use('/db', backend); // add database API methods to /db path
 
-//const frontend = files(__dirname); // Using serve-static to serve webpages with restana
-const frontend = files(path.join(__dirname, 'src'))
-
-service.use('/', frontend) // Use webpages at API root.
-//service.use('/', files(path.join(__dirname, 'public/')))
-
-// service.get('/', async (req, res) => {
-//     fs.readFile('index.html', (err, html) => {
-//         res.statusCode = 200;
-//         res.setHeader('Content-type', 'text/html');
-//         res.write(html);
-//         res.end();
-//         res.
-//     });
-// });
+service.use('/', files(path.join(__dirname, 'src'))); // Serve webpages from src dir.
 
 service.start(port, hostname).then((server) => {
-    console.log(`App server started on http://${hostname}:${port}/`)
+    console.log(`App server started on http://${hostname}:${port}/`);
 });
-
-// const server = http.createServer(service.callback());
-// server.listen(port, hostname, () => {
-//     console.log(`App server started on http://${hostname}:${port}/`);
-// });
--- src/api/db.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-
-exports.connect = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_BASIC_USER,
-    password: process.env.DB_BASIC_PASS,
-    database: process.env.DB_NAME
-});
